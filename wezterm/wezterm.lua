ab6d4b93441612425706fd5b46e1b3e702ff04a4
Kenji Masui <100197646+kenjimasui@users.noreply.github.com>
2024-06-15 03:49:41 +0900

    Adjust display configurations

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme_dirs = { '/Users/kenji/.config/wezterm/colors' }
-config.color_scheme = 'tokyonight_night'
 config.font = wezterm.font_with_fallback {
-  { family = 'NotoSansM Nerd Font Mono' },
+  { family = 'NotoSansM Nerd Font Mono', weight = 'Medium' },
   { family = 'BIZ UDGothic', scale= 1.05 },
 }
 config.font_size = 14.0
 config.line_height = 0.9
-config.initial_rows = 40
+config.initial_rows = 42
 config.initial_cols = 108
+
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 
 return config
